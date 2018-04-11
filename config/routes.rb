90b3ae1c6169f1bebd569ed61815a91ff933f52b
Viktharien <viktharien@zoho.com>
2018-04-11 20:36:42 +0000

    finish action cable implementation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index'
-  post '/chat_rooms/:id'
+  post '/chat_rooms/:id' => 'chat_rooms#create'
   mount ActionCable.server => '/cable'
 end
